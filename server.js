fad20e348cdfbead65095cbd5a3122e4488d0e14
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-03-12 19:03:35 -0500

    added contact

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send('<h1><i>Hello world</i><h1>')
 })
 
+app.get('/contact', (req, res) => {
+    res.send('Call me at: 281-222-2222');
+})
+
 app.listen(3000, (req, res) => {
 
     console.log("listening on port 3000");
